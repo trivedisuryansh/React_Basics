b204bba15f88269f47dd6b7b79d4bd372b33a33b
trivedisuryansh <suryanshtrivedi3@gmail.com>
2025-09-27 15:34:28 +0530

    Update code files

---
+++ src/components/Core_Concepts/CoreConcepts.jsx
@@ -0,0 +1,22 @@
+import { CORE_CONCEPTS } from "../../data";
+import CoreConcept from "./Core_Concepts";
+
+
+export default function CoreConcepts(){
+  return(
+     <section id="core-concepts">
+              <h2>Core Concepts</h2>
+              <ul>
+                {CORE_CONCEPTS.map( (conceptItem)=>  <CoreConcept{...conceptItem}/> )}
+                {/* <CoreConcept 
+                title = {CORE_CONCEPTS[0].title} 
+                description = {CORE_CONCEPTS[0].description}
+                image = {CORE_CONCEPTS[0].image}
+                />
+                <CoreConcept {...CORE_CONCEPTS[1]} />
+                <CoreConcept {...CORE_CONCEPTS[2]} />
+                <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+              </ul>
+            </section>
+  );
+}
